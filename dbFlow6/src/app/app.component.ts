1f6baf5aad3a83a765c19c983a08fa539cde5315
anavarrom <albertonavarro.m@gmail.com>
2018-11-04 19:40:57 +0100

    Test mobile app

--- dbFlow6/src/app/app.component.ts
+++ dbFlow6/src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
 
 import { Page1Component } from './page1/page1.component';
+import { LoginComponent } from './public/login/login.component';
+
+declare var device;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  root = Page1Component;
+export class AppComponent implements  OnInit {
+  root = LoginComponent;
+  ngOnInit() {
+    document.addEventListener('deviceready', function() {
+      alert(device.platform);
+    }, false);
+    }
 }
--- dbFlow6/src/app/public/login/login.component.ts
+++ dbFlow6/src/app/public/login/login.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit                                  } from '@angular/cor
 import { FormBuilder, FormControl, FormGroup, Validators    } from '@angular/forms';
 import { Router                                             } from '@angular/router';
 import { AuthService                                        } from 'ng2-ui-auth';
+import * as ons from 'onsenui';
 
 // Custom services
 import { FormHelperService } from '../../core/services/form-helper.service';
@@ -35,32 +36,34 @@ export class LoginComponent implements OnInit {
   }
 
   login(loginData: { username: string, password: string }) {
-    this.auth.login(loginData)
-      .subscribe({
-        error: (err: any) => alert(err.message),
-        next: (data: any) => {
-            const currentUser = data.tokenInfo;
-            // Update the user store
-            this.userStore.setCurrentUser(currentUser);
-        },
-        complete: () => {
-          this.router.navigateByUrl('main');
-        }
-      });
+    ons.notification.alert('Se lanzo!!!!!!');
+    // this.auth.login(loginData)
+    //   .subscribe({
+    //     error: (err: any) => alert(err.message),
+    //     next: (data: any) => {
+    //         const currentUser = data.tokenInfo;
+    //         // Update the user store
+    //         this.userStore.setCurrentUser(currentUser);
+    //     },
+    //     complete: () => {
+    //       this.router.navigateByUrl('main');
+    //     }
+    //   });
   }
 
   loginWithGoogle() {
-    this.auth.authenticate('google')
-      .subscribe({
-        error: (err: any) => alert(err.message),
-        next: (data: any) => {
-          const currentUser = data.tokenInfo;
-          // Update the user store
-          this.userStore.setCurrentUser(currentUser);
-      },
-      complete: () => {
-          this.router.navigateByUrl('main');
-        }
-      });
+    ons.notification.alert('Se lanzo el logout modificado!!!!!!');
+    // this.auth.authenticate('google')
+    //   .subscribe({
+    //     error: (err: any) => alert(err.message),
+    //     next: (data: any) => {
+    //       const currentUser = data.tokenInfo;
+    //       // Update the user store
+    //       this.userStore.setCurrentUser(currentUser);
+    //   },
+    //   complete: () => {
+    //       this.router.navigateByUrl('main');
+    //     }
+    //   });
   }
 }
